8a2c84cfe60e7aa603e61e006ec63d7e380687f6
spacecaker <robin-barry@hotmail.com>
2014-04-15 20:45:22 +0200

    Updated LifeCompanionLockscreen app.
    Added:
    Color chooser.
    Text Size chooser.
    Changed name confirmation.
    Made the settings app look bit better.
    Made the preference file WORLD readable.

--- src/com/touchwizres/lifecompaniontext/CompanionText.java
+++ src/com/touchwizres/lifecompaniontext/CompanionText.java
@@ -1,58 +1,126 @@
 package com.touchwizres.lifecompaniontext;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class CompanionText extends Activity {
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class CompanionText extends PreferenceActivity {
 	
 	ImageView imageView;
 	ImageView done;
 	EditText name;
+	String comptext;
 	String companionsName;
-	TextView owner;
+	TextView spacecompanion;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.lifercomp);
 		setContentView(R.layout.companion_layout);
 		
-		  owner = (TextView) findViewById(R.id.Owner);
+	  
+		  spacecompanion = (TextView) findViewById(R.id.Owner);
 		  name = (EditText) findViewById(R.id.name_field);
 		  done = (ImageView) findViewById(R.id.name);
 		
 		  //checks shared preferences
-		     SharedPreferences sharedPreferences = getSharedPreferences("LifeCompanionFile",Context.MODE_PRIVATE); 
+		     SharedPreferences sharedPreferences = getSharedPreferences("LifeCompanionFile",Context.MODE_WORLD_READABLE);
+
+		    	String textSize = sharedPreferences.getString("textsizerchange","40");
+		    	((ListPreference)findPreference("textsizerchange")).setSummary(textSize);
+		    	
+		        ((ListPreference)findPreference("textsizerchange")).setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+
+					@Override
+					public boolean onPreferenceChange(Preference preference, Object newValue) {
+						String type = (String.valueOf(newValue));
+						Intent i = new Intent();
+						i.setAction("lifecompanion.CHANGE_COMPANION_SIZE");
+						i.putExtra("textSizer",type);
+						sendBroadcast(i);
+						preference.setSummary(type);
+						SharedPreferences sharedPreferences = getSharedPreferences("LifeCompanionFile",Context.MODE_WORLD_READABLE);
+			            SharedPreferences.Editor editor = sharedPreferences.edit(); //opens the editor
+			            editor.putString("textsizerchange", type); //true or false
+			            editor.commit();			
+						return true;
+					}
+
+		        });
+		    		        
 		     companionsName = sharedPreferences.getString("companionName","null");
 		     if (companionsName == "null") {
-					owner.setText("Life companion"); 					
+		    	 spacecompanion.setText("Life companion"); 					
 			     } else {
-				    	owner.setText(companionsName);
+			    	 	spacecompanion.setText(companionsName);
 				    	name.setText(companionsName); 
 				     };
 		  
 				done.setOnClickListener(new View.OnClickListener() {
 			
 				public void onClick(View v) {
-					Editable profileName = name.getText();
-					owner.setText(profileName.toString());
+					Editable companionName = name.getText();
+					spacecompanion.setText(companionName.toString());
 					Intent intent = new Intent();
 					intent.setAction("lifecompanion.CHANGE_COMPANION");
-					intent.putExtra("NAME", profileName.toString() );
+					intent.putExtra("NAME", companionName.toString() );
 					sendBroadcast(intent);
-			        SharedPreferences sharedPreferences = getSharedPreferences("LifeCompanionFile",Context.MODE_PRIVATE);
+			        SharedPreferences sharedPreferences = getSharedPreferences("LifeCompanionFile",Context.MODE_WORLD_READABLE);
 			        SharedPreferences.Editor editor = sharedPreferences.edit(); //opens the editor
-			        editor.putString("companionName", profileName.toString()); //true or false
-			        editor.commit();
+			        editor.putString("companionName", companionName.toString()); //true or false
+			        editor.commit();			        
+			        try {
+			        	ClickedTextBox("Companion name changed!");			        	
+			        } catch (Exception ex) {
+			        	ClickedTextBox("Companion name not changed!");
+					}
 		        }
-		});
+		});	
+
+		        ((ColorPickerPreference)findPreference("lifecompanionColor")).setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+
+		 			@Override
+		 			public boolean onPreferenceChange(Preference preference, Object newValue) {
+		 				comptext = (ColorPickerPreference.convertToARGB(Integer.valueOf(String.valueOf(newValue))));;
+		 				preference.setSummary(comptext);
+		 	            SharedPreferences sharedPreferences = getSharedPreferences("LifeCompanionFile",Context.MODE_WORLD_READABLE);
+		 	            SharedPreferences.Editor editor = sharedPreferences.edit(); //opens the editor
+		 	            editor.putString("lifecompanionColor", comptext); //true or false
+		 	            editor.commit();
+		 				Intent intent = new Intent();
+		 				intent.setAction("lifecompanion.CHANGE_COMPANION_COLOR");
+		 				intent.putExtra("lifecompanionColor",comptext.toString());
+		 				sendBroadcast(intent);				
+		 				return false;
+		 			}
+
+		         });
+		         String lifecompanioncolor = getSharedPreferences("LifeCompanionFile",Context.MODE_WORLD_READABLE).getString("lifecompanionColor","#ffffffff");
+		 	    
+		 	    ((ColorPickerPreference)findPreference("lifecompanionColor")).setDefaultValue(lifecompanioncolor);
+		 	    ((ColorPickerPreference)findPreference("lifecompanionColor")).setSummary(lifecompanioncolor);				
 	}
+	
+	public void ClickedTextBox(String message) {
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+	}	
 }
 
--- src/com/touchwizres/lifecompaniontext/LifeCompanionTextView.java
+++ src/com/touchwizres/lifecompaniontext/LifeCompanionTextView.java
@@ -1,43 +1,123 @@
 package com.touchwizres.lifecompaniontext;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.provider.Settings;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.TextView;
 
 public class LifeCompanionTextView extends TextView {
 	
-	TextView profilename;
-    String name;
-	public LifeCompanionTextView(final Context context, AttributeSet attrs) {
-	super(context, attrs); 
-	
-		 profilename = (TextView) findViewById(R.id.life_companion_tag);
-	     SharedPreferences sharedPreferences = context.getSharedPreferences("LifeCompanionFile",Context.MODE_PRIVATE);    
-	     name = sharedPreferences.getString("companionName","Life companion");
-     	 profilename.setText(name);
-         Typeface tf = Typeface.createFromFile("/system/fonts/SpaceFont.ttf");
-         profilename.setTypeface(tf);
-
-    BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context c, Intent i) {
-        	name = i.getStringExtra("NAME");
-        	profilename.setText(name);
-            SharedPreferences sharedPreferences = context.getSharedPreferences("LifeCompanionFile",Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit(); //opens the editor
-            editor.putString("companionName", name); //true or false
-            editor.commit();	 
-        }
-        
-    };      
+	TextView lifename;
+	String lifecompColor;
+    String spacename;
+    String textSizer;
+    String companionShow;
+    String message;
     
-    context.registerReceiver(mReceiver, new IntentFilter("lifecompanion.CHANGE_COMPANION"));  
+	public LifeCompanionTextView(final Context context, AttributeSet attrs) {
+	super(context, attrs);
+	
+		// id0value
+       	lifename = (TextView) findViewById(R.id.life_companion_tag);    
+       	
+       	// preference
+	    SharedPreferences sharedPreferences = context.getSharedPreferences("LifeCompanionFile",Context.MODE_WORLD_READABLE);
+	    
+	    // textchanger
+	    spacename = sharedPreferences.getString("companionName","Life companion");
+	    lifename.setText(spacename);
+	    
+	    // color
+    	lifecompColor = sharedPreferences.getString("lifecompanionColor","#ffffffff");
+    	lifename.setTextColor(Color.parseColor(lifecompColor));
+    		    
+	    // font
+        Typeface tf = Typeface.createFromFile("/system/fonts/SpaceFont.ttf");
+        lifename.setTypeface(tf);
+	    
+        // options
+	    textSizer = sharedPreferences.getString("type","40");
+	     	if ("40".equals(textSizer)) {
+	     		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
+	    	}
+	    	else if ("30".equals(textSizer)) {
+	    		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+	    	} 
+	    	else if ("20".equals(textSizer)) {
+	    		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+	    	}
+	    	else if ("15".equals(textSizer)) {
+	    		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+	    	}
+	    	else if ("10".equals(textSizer)) {
+	    		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
+	    	}		     	
+	    	else if ("5".equals(textSizer)) {
+	    		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 5);
+	    	}	     	
+	     		        
+	        BroadcastReceiver mTextSizerReceiver = new BroadcastReceiver() {
+	            @Override
+	            public void onReceive(Context c, Intent i) {
+	            	message = i.getStringExtra("textSizer");
+	            	
+	            	if ("40".equals(message )) {
+	            		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40);
+	            	}
+	            	else if ("30".equals(message )) {
+	            		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+	            	}
+	    	    	else if ("20".equals(message)) {
+	    	    		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+	    	    	}
+	    	    	else if ("15".equals(message)) {
+	    	    		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+	    	    	}
+	    	    	else if ("10".equals(message)) {
+	    	    		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
+	    	    	}	
+	    	    	else if ("5".equals(message)) {
+	    	    		lifename.setTextSize(TypedValue.COMPLEX_UNIT_SP, 5);
+	    	    	}	            	
+	            }
+	            
+	        };  
+	        
+	    BroadcastReceiver mLifeColorReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent i) {
+            	lifecompColor = i.getStringExtra("lifecompanionColor");
+          	  	lifename.setTextColor(Color.parseColor(lifecompColor));
+          	  	SharedPreferences sharedPreferences = context.getSharedPreferences("LifeCompanionFile",Context.MODE_WORLD_READABLE);
+              	SharedPreferences.Editor editor = sharedPreferences.edit(); //opens the editor
+              	editor.putString("lifecompanionColor", lifecompColor); //true or false
+              	editor.commit();	 
+            }            
+        };
+        
+        BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        	@Override
+        	public void onReceive(Context c, Intent i) {
+        		spacename = i.getStringExtra("NAME");
+        		lifename.setText(spacename);
+        		SharedPreferences sharedPreferences = context.getSharedPreferences("LifeCompanionFile",Context.MODE_WORLD_READABLE);
+        		SharedPreferences.Editor editor = sharedPreferences.edit(); //opens the editor
+        		editor.putString("companionName", spacename); //true or false
+        		editor.commit();	 
+        }       
+    };   
+    
+    context.registerReceiver(mTextSizerReceiver, new IntentFilter("lifecompanion.CHANGE_COMPANION_SIZE"));   
+    context.registerReceiver(mLifeColorReceiver, new IntentFilter("lifecompanion.CHANGE_COMPANION_COLOR"));
+    context.registerReceiver(mReceiver, new IntentFilter("lifecompanion.CHANGE_COMPANION"));    
 	}
-
-
 }
